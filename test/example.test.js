2c99b72cb04874f9e17685e72b567e46a3628db1
Benatovich <sambenatovich@gmail.com>
2022-01-12 20:25:01 -0500

    tests are passing, logout issue is fixed

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,41 @@
+/* eslint-disable no-useless-escape */
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import {
+    renderGame,
+    renderTeam
+} from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('should return a DOM node for a team', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<div class=\"team\"><p class=\"name\">chelsea</p><p class=\"score\">3</p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderTeam('chelsea', 3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
+});
+test('should return a DOM node for a game', (expect) => {
+    //Arrange
+    let currentGame = {
+        name1: '',
+        name2: '',
+        score1: 0,
+        score2: 0,
+    }; 
+    // Set up your arguments and expectations
+    const expected = '<div class=\"game\"><div class=\"team\"><p class=\"name\"></p><p class=\"score\">0</p></div><div class=\"team\"><p class=\"name\"></p><p class=\"score\">0</p></div></div>';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderGame(currentGame);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
 });
